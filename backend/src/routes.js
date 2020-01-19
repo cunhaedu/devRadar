ca7b9ee1de7633e5310235d31694f3d3919fd82d
Eduardo Assuncao <cunhaeduardo1231@gmail.com>
2020-01-19 09:25:40 -0300

    inicializando

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
+const routes = Router();
+
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+routes.get('/search', SearchController.index);
+
+module.exports = routes;
+
+
+//Métodos HTTP: GET, POST, PUT, DELETE
+
+/* Tipos de parâmetros:
+ * Query Params: request.query (Filtros, ordenação, paginação...)
+ * Route Params: request.params (Identificar um recurso na alteração ou remoção)
+ * Body: request.body (dados para a criação ou alteração de um registro)
+ */
